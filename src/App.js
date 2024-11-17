8afbd9d4f9006941342667e10c694cb3db4e9567
Angelo <angelojelo@outlook.com>
2024-11-17 00:13:40 +0000

    inital push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Sidebar';
+import Login from './Pages/Login';
 function App() {
   return (
     <>
-    <Sidebar/>
+    <Login/>
     </>
   );
 }
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div className="text-5xl text-center">
+        hello Login
+        </div>
+    )
+}
+
+export default Login
